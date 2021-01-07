21318d7af031ec658abf0f2c712af21a22f4950e
Chris Novick <c.r.novick@gmail.com>
2021-01-06 20:27:49 -0500

    Minor cleanup

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -321,25 +321,26 @@ fn public_key(ctx: &Context) -> Result<VerifyingKey> {
 }
 
 const NUM_ADDRS: usize = 5;
-fn addresses(ctx: &mut Context) -> Result<[u8; 20 * NUM_ADDRS]> {
-    let idx = ctx.idx;
-    let mut buf = [0u8; 20 * NUM_ADDRS];
-    for i in idx..idx + NUM_ADDRS as u32 {
-        let start: usize = (i - idx) as usize * 20;
-        let stop: usize = (i - idx) as usize * 20 + 20;
-        buf[start..stop].copy_from_slice(&address(ctx.set_idx(i))?);
+const ADDR_SIZE: usize = 20;
+fn addresses(ctx: &mut Context) -> Result<[u8; ADDR_SIZE * NUM_ADDRS]> {
+    let idx = ctx.idx as usize;
+    let mut buf = [0u8; ADDR_SIZE * NUM_ADDRS];
+    for i in idx..idx + NUM_ADDRS {
+        let start: usize = (i - idx) * ADDR_SIZE;
+        let stop: usize = start + ADDR_SIZE; // Addresses are 20 bytes
+        buf[start..stop].copy_from_slice(&address(ctx.set_idx(i as u32))?);
     }
     Ok(buf)
 }
 
-fn address(ctx: &Context) -> Result<[u8; 20]> {
+fn address(ctx: &Context) -> Result<[u8; ADDR_SIZE]> {
     let uncompressed_pubkey = public_key(ctx)?.to_encoded_point(false);
     let pubkey_bytes = uncompressed_pubkey.as_bytes();
     let mut hasher = Keccak::v256();
     hasher.update(&pubkey_bytes[1..]);
     let mut buf = [0u8; 32];
     hasher.finalize(&mut buf);
-    let mut address = [0u8; 20];
+    let mut address = [0u8; ADDR_SIZE];
     address.copy_from_slice(&buf[12..]);
     Ok(address)
 }
